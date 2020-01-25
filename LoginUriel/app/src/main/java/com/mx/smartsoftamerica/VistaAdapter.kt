0178c12956cc5b2d2ea6e15e19f595d5de6663a9
José Uriel Murrieta García <uri.esgas@gmail.com>
2020-01-25 09:36:17 -0600

    listaAdapter

--- LoginUriel/app/src/main/java/com/mx/smartsoftamerica/VistaAdapter.kt
+++ LoginUriel/app/src/main/java/com/mx/smartsoftamerica/VistaAdapter.kt
@@ -1,11 +1,23 @@
 package com.mx.smartsoftamerica
 
+import android.content.Context
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.mx.smartsoftamerica.bo.Usuario
 
-class VistaAdapter(val lista: MutableList<Usuario>, val interfaz : VistaInterface) {
+class VistaAdapter(val lista: MutableList<Usuario>, val context : Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = lista.get(position)
+        holder.
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
